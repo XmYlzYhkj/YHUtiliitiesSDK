fd8a250ad2d8146f2e038a83cc7980b0bfab750e
oid <42318168@qq.com>
2018-06-10 17:23:38 +0800

    add V1.0.1

---
+++ Frameworks/YHUtiliitiesSDK.framework/Headers/YHUtiliitiesSDK.h
@@ -0,0 +1,27 @@
+//
+//  YHUtiliitiesSDK.h
+//  YHUtiliitiesSDK
+//
+//  Created by zxl on 2018/1/16.
+//  Copyright © 2018年 YH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YHUtiliitiesSDK.
+FOUNDATION_EXPORT double YHUtiliitiesSDKVersionNumber;
+
+//! Project version string for YHUtiliitiesSDK.
+FOUNDATION_EXPORT const unsigned char YHUtiliitiesSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YHUtiliitiesSDK/PublicHeader.h>
+
+#import <YHUtiliitiesSDK/YHDataHelper.h>
+#import <YHUtiliitiesSDK/YHDeviceHelper.h>
+#import <YHUtiliitiesSDK/YHFeedBackHelper.h>
+
+#import <YHUtiliitiesSDK/YHGestureHelper.h>
+#import <YHUtiliitiesSDK/YHPredicateHelper.h>
+#import <YHUtiliitiesSDK/YHViewHelper.h>
+#import <YHUtiliitiesSDK/YHImageHelper.h>
+
