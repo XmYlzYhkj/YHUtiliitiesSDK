761a989734e6034d44d9213de8a1005f8e032b9e
liaojiangtu <42318168@qq.com>
2019-12-14 21:12:31 +0800

    feat:add YHEnvDetectionHelper\YHVcHelper

---
+++ Frameworks/YHUtiliitiesSDK.framework/Headers/YHEnvDetectionHelper.h
@@ -0,0 +1,16 @@
+//
+//  YHEnvDetectionHelper.h
+//  YHUtiliitiesSDK
+//  Environmental or Jailbreak Detection
+//  Created by Jagtu on 2019/1/3.
+//  Copyright © 2019年 YH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YHEnvDetectionHelper : NSObject
+
+//是否未越狱
++ (BOOL)isOriginal;
+
+@end
---
+++ Frameworks/YHUtiliitiesSDK.framework/Headers/YHVcHelper.h
@@ -0,0 +1,27 @@
+//
+//  YHVcHelper.h
+//  YHRongYiTong
+//
+//  Created by zxl on 2017/11/20.
+//  Copyright © 2017年 YH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface YHVcHelper : NSObject
+
+/*
+ 控制器
+ */
++(UINavigationController *)getCurrentNav;
+
++(UIViewController *)getCurrentVc;
+
++(UIViewController *)getRootVC;
+
+//弃用
+//+(void)gotoNextVC:(UIViewController *)controller;
+
+
+@end
