c8126888f27dc809e5e82eb5e3086a8258b989a2
liaojiangtu <42318168@qq.com>
2019-12-14 19:59:19 +0800

    feat:1.0.3

--- Frameworks/YHUtiliitiesSDK.framework/Headers/YHUtiliitiesSDK.h
+++ Frameworks/YHUtiliitiesSDK.framework/Headers/YHUtiliitiesSDK.h
@@ -24,4 +24,5 @@ FOUNDATION_EXPORT const unsigned char YHUtiliitiesSDKVersionString[];
 #import <YHUtiliitiesSDK/YHPredicateHelper.h>
 #import <YHUtiliitiesSDK/YHViewHelper.h>
 #import <YHUtiliitiesSDK/YHImageHelper.h>
+#import <YHUtiliitiesSDK/YHEnvDetectionHelper.h>
 
